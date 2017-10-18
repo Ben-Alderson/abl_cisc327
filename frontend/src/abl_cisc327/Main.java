8cece17fc16548619bf205f8b7062e39b9f9049d
Ben Alderson <bennyalderson@gmail.com>
2017-10-18 09:52:56 -0400

    Add withdraw, validateAmount, validateName and clean up code

--- frontend/src/abl_cisc327/Main.java
+++ frontend/src/abl_cisc327/Main.java
@@ -2,7 +2,7 @@ package abl_cisc327;
 
 import java.util.Scanner;
 
-//The Main class is to parse and execute commands and core functions of the program.
+// The Main class is to parse and execute commands and core functions of the program.
 public class Main {
 	
 	static boolean isUserLoggedin = false;
@@ -11,13 +11,13 @@ public class Main {
 	static TransactionSummaryFile transactions;
 	static Scanner input;
 	
-	//The purpose of this function is to call on all the 
-	//essential methods (withdraw, transfer) and classes (TransactionSummaryFile, ValidAccountsFile). 
+	// The purpose of this function is to call on all the 
+	// essential methods (withdraw, transfer) and classes (TransactionSummaryFile, ValidAccountsFile). 
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
 		
 		validAccounts = new ValidAccountsFile(args[1]);
-		transactions = new TransactionsSummaryFile(args[2]);
+		transactions = new TransactionSummaryFile(args[2]);
 
 		while(true) {
 			System.out.print("Enter command: ");
@@ -55,13 +55,13 @@ public class Main {
 					System.out.println("ERROR: Invalid command");
 					break;
 				}
-			}catch(Exception e){
+			} catch(Exception e) {
 				continue;
 			}
 		}
 	}
 	
-	//Makes sure first thing the user sees on screen is a login page.
+	// Makes sure first thing the user sees on screen is a login page.
 	private static void login() {
 		System.out.print("Enter login: ");
 		String user = input.nextLine().toLowerCase();
@@ -85,7 +85,7 @@ public class Main {
 		}
 	}
 	
-	//User exits the session and gets the transaction summary file.
+	// User exits the session and gets the transaction summary file.
 	private static void logout() {
 		if(!isUserLoggedin) {
 			// Logout should only be accepted when logged in
@@ -97,11 +97,13 @@ public class Main {
 		isUserAgent = false;
 	}
 	
-	//When taking out specified amount of money, 
-	//the method ensures the amount is correct and it is not more than the limit. 
-	private static void withdraw(){
+	// When taking out specified amount of money, 
+	// the method ensures the amount is correct and it is not more than the limit. 
+	private static void withdraw() throws Exception {
+		System.out.print("Enter the account number to transfer to: ");
+		String accountNumberStr = input.nextLine();
 		System.out.print("Enter amount to withdraw: ");
-		String amount = input.nextLine();
+		String amountStr = input.nextLine();
 		
 		
 		if(!isUserLoggedin) {
@@ -110,19 +112,20 @@ public class Main {
 			return;
 		}
 		
-		int withdrawAmount = Integer.parseInt(amount);
-		if(withdrawAmount < 0 || withdrawAmount > 1000){
-			//specification of limit
-			System.out.println("Can't withdraw money below 0 or over 1000 in ATM mode");
+		int accountNumber = validateAccountNumber(accountNumberStr);
+		int withdrawAmount = validateAmount(amountStr);
+		
+		if(!validAccounts.isValid(accountNumber)) {
+			System.out.println("ERROR: invalid account number");
+			return;
 		}
-		else{
-			transactions.addCommand("WDR",accountNumber,withdrawAmount,0,null);
-			System.out.println(""You have successfully tranferred $%.2f to %d", ((float) transferAmount)/100, fromAccountNumber");
-		}
+		
+		transactions.addCommand("WDR", 0, withdrawAmount, accountNumber, null);
+		System.out.printf("You have successfully tranferred $%.2f to %d\n", ((float) withdrawAmount)/100, accountNumber);
 	}
 	
-	//Ensures the accounts information and amount transferred are correct 
-	//before proceeding with the process.
+	// Ensures the accounts information and amount transferred are correct 
+	// before proceeding with the process.
 	private static void transfer() throws Exception{
 		System.out.println("Enter the account number to transfer from");
 		String fromAccountNumberStr = input.nextLine();
@@ -142,37 +145,20 @@ public class Main {
 		int fromAccountNumber = validateAccountNumber(fromAccountNumberStr);
 		int toAccountNumber = validateAccountNumber(toAccountNumberStr);
 		
-		//check for atm mode and amount
-		if((transferAmount<0 || transferAmount > 100000)){
-			System.out.println("ERROR: Can't transfer more than $1,000.00");
-			return;
-		}
-		else if((transferAmount<0 || transferAmount > 99999999) && !isUserAgent){
-			System.out.println("ERROR: Can't transfer more than $999,999.99");
-			return;
-		}
-	
-		
 		//check if accounts exist 
-		if(!validAccounts.isValid(fromAccountNumber) || !validAccounts.isValid(toAcountNumber)){
+		if(!validAccounts.isValid(fromAccountNumber) || !validAccounts.isValid(toAccountNumber)){
 			System.out.println("ERROR: invalid account number");
 			return;
 		}
 		
-		transactions.addCommand ("XFR",toAccountNumber, transferAmount, fromAccountNumber,null);
-		System.out.printf("You have successfully tranferred $%.2f to %d", ((float) transferAmount)/100, toAccountNumber);
-		
-		
-		//working case
-		
-		
-		
+		transactions.addCommand("XFR", toAccountNumber, transferAmount, fromAccountNumber, null);
+		System.out.printf("You have successfully tranferred $%.2f to %d\n", ((float) transferAmount)/100, toAccountNumber);
 	}
 	
-	//Allows agent to create an account.
-	private static void createacct() {
-		String inputNumber = input.nextLine();
-		String inputName = input.nextLine();
+	// Allows agent to create an account.
+	private static void createacct() throws Exception {
+		String accountNumberStr = input.nextLine();
+		String accountNameStr = input.nextLine();
 		
 		if(!isUserLoggedin) {
 			// No transaction (createacct) other than login should be accepted before login
@@ -185,20 +171,39 @@ public class Main {
 			return;
 		}
 		
-		int number;
-		String name;
-		try {
-			number = validateAccountNumber(inputNumber);
-			name = validateAccountName(inputName);
-		} catch (Exception e) {
+		int accountNumber = validateAccountNumber(accountNumberStr);
+		String accountName = validateAccountName(accountNameStr);
+		
+		transactions.addCommand("NEW", accountNumber, 0, 0, accountName);
+		System.out.printf("You have successfully created a new account\n");
+	}
+	
+	// Allows agent to create an account.
+	private static void deleteacct() throws Exception {
+		String accountNumberStr = input.nextLine();
+		String accountNameStr = input.nextLine();
+		
+		if(!isUserLoggedin) {
+			// No transaction (deleteacct) other than login should be accepted before login
+			System.out.println("ERROR: You need to login first");
+			return;
+		}
+		else if(!isUserAgent) {
+			// After atm login, only unprivileged transactions are accepted
+			System.out.println("ERROR: You cannot delete an account");
 			return;
 		}
 		
-		// TODO: Process transaction
+		int accountNumber = validateAccountNumber(accountNumberStr);
+		String accountName = validateAccountName(accountNameStr);
+		
+		transactions.addCommand("DEL", accountNumber, 0, 0, accountName);
+		System.out.printf("Account deleted\n");
 	}
 		
 	
-	//Allows agents to delete an account
+	// Verifies the account number. It takes a String as a parameter and returns an integer.
+	// If it is not valid an exception is thrown.
 	private static int validateAccountNumber(String number) throws Exception {
 		int accountNumber;
 		
@@ -219,8 +224,64 @@ public class Main {
 		return accountNumber;
 	}
 	
-	private static String validateAccountName(String name) {
-		// TODO: Validate the account name
+	// Verifies the account name. It takes a String as a parameter and returns a String.
+	// If it is not valid, an exception is thrown.
+	private static String validateAccountName(String name) throws Exception {
+		if(name.length() < 3) {
+			System.out.println("ERROR: Your account name must be between 3 and 30 alphanumeric characters and not begin or end with a space");
+			throw new Exception();
+		}
+		else if(name.length() > 30) {
+			System.out.println("ERROR: Your account name must be between 3 and 30 alphanumeric characters and not begin or end with a space");
+			throw new Exception();
+		}
+		else if(name.startsWith(" ") || name.endsWith(" ")) {
+			System.out.println("ERROR: Your account name must not start or end with a space");
+			throw new Exception();
+		}
+		for(int i=0; i<name.length(); i++) {
+			char c = name.charAt(i);
+			boolean isAlpha = (c >= 'a' && c <= 'z')
+					|| (c >= 'A' && c <= 'Z')
+					|| (c >= '0' && c <= '9')
+					|| c == ' ';
+			if(!isAlpha) {
+				System.out.println("ERROR: Your account name must be alphanumeric");
+				throw new Exception();
+			}
+		}
+		
 		return name;
+	}
+	
+	// Verifies that the amount of money being either deposited, withdrawn
+	// or transferred for both ATM and agent modes is valid.
+	// It takes a String as a parameter and returns an int.
+	// If amount is over the allowed limit, an exception is thrown.
+	private static int validateAmount(String amountStr) throws Exception {
+		int amount;
+		try {
+			amount = Integer.parseInt(amountStr, 10);
+		} catch(NumberFormatException e) {
+			// Amount must be a number
+			System.out.println("ERROR: Amount must be a number");
+			throw new Exception();
+		}
+		
+		//check for atm mode and amount
+		if(amount < 0) {
+			System.out.println("ERROR: Can't transfer more than $1,000.00");
+			throw new Exception();
+		}
+		else if(amount > 100000 && !isUserAgent){
+			System.out.println("ERROR: Can't transfer more than $1,000.00");
+			throw new Exception();
+		}
+		else if(amount > 99999999){
+			System.out.println("ERROR: Can't transfer more than $999,999.99");
+			throw new Exception();
+		}
+		
+		return amount;
 	}
 }
