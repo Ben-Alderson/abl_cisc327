e66fa827a32f16e5819f16b670c81af018569506
Ben Alderson <bennyalderson@gmail.com>
2017-10-18 20:33:42 -0400

    Small changes for submission.

--- frontend/src/abl_cisc327/Main.java
+++ frontend/src/abl_cisc327/Main.java
@@ -108,6 +108,7 @@ public class Main {
 		isUserLoggedin = false;
 		isUserAgent = false;
 		
+		// End session and write to the transaction file
 		transactions.addCommand("EOS", 0, 0, 0, null);
 		try {
 			transactions.flush();
@@ -139,18 +140,16 @@ public class Main {
 		}
 		
 		transactions.addCommand("DEP", depositAccount, depositAmount, 0, null);
-		System.out.printf("You have successfully deposited $%.2f to %d", ((float) depositAmount)/100, depositAccount);
+		System.out.printf("You have successfully deposited $%.2f to %d\n", ((float) depositAmount)/100, depositAccount);
 	}
 	
 	// When taking out specified amount of money, 
 	// the method ensures the amount is correct and it is not more than the limit. 
 	private static void withdraw() throws Exception {
-		System.out.print("Enter the account number to transfer to: ");
+		System.out.print("Enter the account to withdraw: ");
 		String accountNumberStr = input.nextLine();
 		System.out.print("Enter amount to withdraw: ");
 		String amountStr = input.nextLine();
-		
-		
 		
 		if(!isUserLoggedin) {
 			// No transaction (withdraw) other than login should be accepted before login
@@ -167,17 +166,17 @@ public class Main {
 		}
 		
 		transactions.addCommand("WDR", 0, withdrawAmount, accountNumber, null);
-		System.out.printf("You have successfully tranferred $%.2f to %d\n", ((float) withdrawAmount)/100, accountNumber);
+		System.out.printf("You have successfully withdrawn $%.2f to %d\n", ((float) withdrawAmount)/100, accountNumber);
 	}
 	
 	// Ensures the accounts information and amount transferred are correct 
 	// before proceeding with the process.
-	private static void transfer() throws Exception{
-		System.out.println("Enter the account number to transfer from");
+	private static void transfer() throws Exception {
+		System.out.print("Enter the account number to transfer from: ");
 		String fromAccountNumberStr = input.nextLine();
-		System.out.println("Enter the account number to transfer to");
+		System.out.print("Enter the account number to transfer to: ");
 		String toAccountNumberStr = input.nextLine();
-		System.out.println("Enter amount to transfer");
+		System.out.print("Enter amount to transfer: ");
 		String amount = input.nextLine();
 	
 		
@@ -203,7 +202,9 @@ public class Main {
 	
 	// Allows agent to create an account.
 	private static void createacct() throws Exception {
+		System.out.print("Enter the new account's number: ");
 		String accountNumberStr = input.nextLine();
+		System.out.print("Enter the new account's name: ");
 		String accountNameStr = input.nextLine();
 		
 		if(!isUserLoggedin) {
@@ -220,13 +221,20 @@ public class Main {
 		int accountNumber = validateAccountNumber(accountNumberStr);
 		String accountName = validateAccountName(accountNameStr);
 		
+		if(validAccounts.isValid(accountNumber)) {
+			System.out.println("ERROR: Your account number must be different from other current account numbers");
+			return;
+		}
+		
 		transactions.addCommand("NEW", accountNumber, 0, 0, accountName);
 		System.out.printf("You have successfully created a new account\n");
 	}
 	
 	// Allows agent to create an account.
 	private static void deleteacct() throws Exception {
+		System.out.print("Enter the target account's number: ");
 		String accountNumberStr = input.nextLine();
+		System.out.print("Enter the target account's name: ");
 		String accountNameStr = input.nextLine();
 		
 		if(!isUserLoggedin) {
@@ -244,6 +252,7 @@ public class Main {
 		String accountName = validateAccountName(accountNameStr);
 		
 		transactions.addCommand("DEL", accountNumber, 0, 0, accountName);
+		validAccounts.delete(accountNumber);
 		System.out.printf("Account deleted\n");
 	}
 		
