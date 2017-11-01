43da9d12585ebe1eece85b22d316575c973cc199
Ben Alderson <bennyalderson@gmail.com>
2017-11-01 10:57:26 -0400

    Fix test script to work on more terminal emulators. Make it actually validate the transaction_summary file.

--- frontend/tests/run.sh
+++ frontend/tests/run.sh
@@ -5,8 +5,6 @@ GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
 RESET="\033[0;0m"
 
-echo
-
 # Clean up from previous runs
 rm -r output
 
@@ -14,7 +12,7 @@ rm -r output
 for folder in $(ls -v input); do
 	# Create the output folder
 	mkdir -p output/$folder
-	echo -e "${RESET}Running test ${YELLOW}$folder${RESET}\033[s"
+	echo -ne "${RESET}Running test ${YELLOW}$folder${RESET} - ${RED}"
 
 	# Invoke the program
 	java -cp ../bin/ abl_cisc327.Main input/$folder/valid_accounts.txt output/$folder/transaction_summary.log < input/$folder/in.txt > output/$folder/out.log
@@ -24,7 +22,7 @@ for folder in $(ls -v input); do
 	OUTPUT_CODE=$?
 
 	# Check that we actually have a file
-	if [ -f expected/$folder/transaction_summary.txt ] && ! [ -f output/$folder/transaction_summary.log ]; then
+	if [ -f expected/$folder/transaction_summary.txt ]; then
 		diff expected/$folder/transaction_summary.txt output/$folder/transaction_summary.log
 		TRANSACTION_CODE=$?
 	else
@@ -33,6 +31,6 @@ for folder in $(ls -v input); do
 
 	# Output success if the test passed.
 	if [ $TRANSACTION_CODE == 0 ] && [ $OUTPUT_CODE == 0 ]; then
-		echo -e "\033[u\033[A${RESET} - ${GREEN}Passed"
+		echo -e "${GREEN}Passed"
 	fi
 done
