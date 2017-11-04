b6df048f3c0c6a659c240a05d653c4f779a763da
Ben Alderson <bennyalderson@gmail.com>
2017-11-04 18:45:58 -0400

    Split validate.sh and run.sh test scripts.

--- frontend/tests/run.sh
+++ frontend/tests/run.sh
@@ -12,25 +12,8 @@ rm -r output
 for folder in $(ls -v input); do
 	# Create the output folder
 	mkdir -p output/$folder
-	echo -ne "${RESET}Running test ${YELLOW}$folder${RESET} - ${RED}"
+	echo -e "${RESET}Running test ${YELLOW}$folder${RESET}"
 
 	# Invoke the program
 	java -cp ../bin/ abl_cisc327.Main input/$folder/valid_accounts.txt output/$folder/transaction_summary.log < input/$folder/in.txt > output/$folder/out.log
-
-	# Check the output
-	diff expected/$folder/out.txt output/$folder/out.log
-	OUTPUT_CODE=$?
-
-	# Check that we actually have a file
-	if [ -f expected/$folder/transaction_summary.txt ]; then
-		diff expected/$folder/transaction_summary.txt output/$folder/transaction_summary.log
-		TRANSACTION_CODE=$?
-	else
-		TRANSACTION_CODE=0
-	fi
-
-	# Output success if the test passed.
-	if [ $TRANSACTION_CODE == 0 ] && [ $OUTPUT_CODE == 0 ]; then
-		echo -e "${GREEN}Passed"
-	fi
 done
---
+++ frontend/tests/validate.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+RESET="\033[0;0m"
+
+THERE_WERE_PROBLEMS=0
+echo
+
+for folder in $(ls -v input); do
+    # Check the terminal output
+    diff -s expected/$folder/out.txt output/$folder/out.log > /dev/null
+    OUTPUT_CODE=$?
+
+    # Print out the diff if it failed
+    if [ $OUTPUT_CODE != 0 ]; then
+        echo -e "${YELLOW}$folder${RESET} - Terminal output didn't match: ${RED}"
+        diff expected/$folder/out.txt output/$folder/out.log
+        echo -ne "${RESET}"
+        THERE_WERE_PROBLEMS=1
+        echo
+    fi
+
+    # Check that we actually have a file
+    if [ -f expected/$folder/transaction_summary.txt ]; then
+        diff expected/$folder/transaction_summary.txt output/$folder/transaction_summary.log > /dev/null
+        TRANSACTION_CODE=$?
+    else
+        TRANSACTION_CODE=0
+    fi
+
+    if [ $TRANSACTION_CODE != 0 ]; then
+        echo -e "${YELLOW}$folder${RESET} - Transaction summary didn't match: ${RED}"
+        diff expected/$folder/transaction_summary.txt output/$folder/transaction_summary.log
+        echo -ne "${RESET}"
+        THERE_WERE_PROBLEMS=1
+        echo
+    fi
+done
+
+if [ $THERE_WERE_PROBLEMS == 0 ]; then
+    echo -e "${GREEN}All tests passed${RESET}"
+else
+    echo -e "Some tests didn't pass${RESET}"
+fi
